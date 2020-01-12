281913507ca1096049f5556c332d05a96b870866
victorcarinanos <victor.carinanos92@gmail.com>
2020-01-12 12:05:52 +0100

    Añado las rutas para administrar los productos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,11 +7,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
+import { AdminProductosComponent } from './components/admin-productos/admin-productos.component';
 
 // Array con la configuración de las rutas
 const appRoutes: Routes = [
-	{path: '', component: HomeComponent},
+	{path: 'inicio', component: HomeComponent},
 	{path: 'login', component: LoginComponent},
+	{path: 'nuevo-producto', component: AdminProductosComponent},
+	{path: 'modificar-producto/:idProducto', component: AdminProductosComponent},
 	{path: ':categoria', component: CategoriaComponent},
 	{path: ':categoria/:idProducto', component: ProductoComponent},
 	{path: '**', component: HomeComponent}	//404
