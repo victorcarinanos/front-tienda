73ed18b7a41b5fa3caa2631c43e1fd3d63ef8a6a
victorcarinanos <victor.carinanos92@gmail.com>
2020-01-12 12:06:58 +0100

    Importo las dependencias al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { UsuarioService } from './services/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +13,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ProductosDestacadosComponent } from './components/productos-destacados/productos-destacados.component';
+import { AdminProductosComponent } from './components/admin-productos/admin-productos.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +22,20 @@ import { CategoriaComponent } from './components/categoria/categoria.component';
     HomeComponent,
     LoginComponent,
     ProductoComponent,
-    CategoriaComponent
+    CategoriaComponent,
+    ProductosDestacadosComponent,
+    AdminProductosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-	routing
+	routing,
+	FormsModule,
+	HttpClientModule,
+	NgxPaginationModule
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
